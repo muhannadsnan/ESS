e7c8c737a5a949a322636e3eb849c064d346d8b2
muhannadsnan <msn-23@live.com>
2018-04-14 04:16:10 +0200

    logo-white & logo-black

--- scripts/custom.js
+++ scripts/custom.js
@@ -2,7 +2,15 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     var doIt = 0;
     $("#sectionsNav").removeClass("navbar-transparent");
+    $("#sectionsNav").removeClass("navbar-transparent");
+
     if (scroll < 50) {
         $("#sectionsNav").addClass("navbar-transparent");
+
+        $(".logo-black").hide();
+        $(".logo-white").show();
+    } else {
+        $(".logo-black").show();
+        $(".logo-white").hide();
     }
 });
